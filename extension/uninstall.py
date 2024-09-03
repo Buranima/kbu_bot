b5094cad33e58917a604d42c99da90f381a3f032
Buranima <lee123021@gmail.com>
2024-09-04 01:36:50 +0700

    add extension
    - เพิ่มไฟล์สำหรับติดตั้งและถอนการติดตั้งไลบรารีที่ใช้สำหรับ KBU Bot
    - เพิ่มไฟล์กำหนดค่าสำหรับไลบรารี

---
+++ extension/uninstall.py
@@ -0,0 +1,15 @@
+import json
+import subprocess
+import sys
+
+# อ่านไฟล์ JSON
+with open('extension/extension_install.json', 'r') as file:
+    data = json.load(file)
+
+# ตรวจสอบว่าใน JSON มีคีย์ "extension_install" หรือไม่
+if "extension_install" in data:
+    packages = data["extension_install"]
+    
+    # ติดตั้งไลบรารีทั้งหมดที่ระบุในรายการ
+    for package in packages:
+        subprocess.check_call([sys.executable, "-m", "pip", "uninstall", package])
