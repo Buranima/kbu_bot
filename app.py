cd6f7d8cd3138c0dbd951e2c941281d88ea66bd3
Buranima <lee123021@gmail.com>
2024-09-16 23:32:13 +0700

    all update

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ import mysql.connector
 import json
 import os
 
-folders = ['static/sound', 'static/base']
+folders = ['static/sound', 'static/base/sound']
+
+allowed_extensions = {'.mp3'}
 
 file_list = []
 
 for folder in folders:
-    file_list.extend([f"{folder}/{file_name}" for file_name in os.listdir(folder)])
+    for file_name in os.listdir(folder):
+        _, extension = os.path.splitext(file_name)
+        if extension.lower() in allowed_extensions:
+            file_list.append(f"{folder}/{file_name}")
 
 sound_list = {"sound_list": file_list}
 
