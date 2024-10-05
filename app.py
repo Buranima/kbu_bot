c0dd275b5b799e9864e9909fc9e2b146c6ebca48
Buranima <lee123021@gmail.com>
2024-10-05 22:19:34 +0700

    all update

--- app.py
+++ app.py
@@ -1,26 +1,7 @@
 from flask import Flask, render_template
-import mysql.connector
-import json
-import os
+from load_sound import loadSound
 
-folders = ['static/sound', 'static/base/sound']
-
-allowed_extensions = {'.mp3'}
-
-file_list = []
-
-for folder in folders:
-    for file_name in os.listdir(folder):
-        _, extension = os.path.splitext(file_name)
-        if extension.lower() in allowed_extensions:
-            file_list.append(f"{folder}/{file_name}")
-
-sound_list = {"sound_list": file_list}
-
-json_file_path = 'static/temp/sound_list.json'
-
-with open(json_file_path, 'w') as json_file:
-    json.dump(sound_list, json_file, indent=4)
+loadSound()
 
 app = Flask(__name__)
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,37 @@
-document.getElementById("eye_image").style.opacity = "0";
-// document.getElementById("eye_image").style.opacity = "1";
+var wake_word = new webkitSpeechRecognition();
+wake_word.lang = "th-TH";
+wake_word.interimResults = true;
+wake_word.continuous = false;
+wake_word.maxAlternatives = 1;
 
-var audio = document.getElementById('startAudio');
-audio.volume = 0.7;
-audio.play();
-
+function resetWakeWord() {
+    wake_word.onend = function () {
+        wake_word.start();
+    }
+    wake_word.onresult = function (event) {
+        for (var event_wake_word = event.resultIndex; event_wake_word < event.results.length; ++event_wake_word) {
+            var speechRecognitionAlternative1 = event.results[event_wake_word][0];
+            var tranScript1 = speechRecognitionAlternative1.transcript;
+            var newtranScript1 = tranScript1.replace("ครับ", "");
+            newtranScript1 = newtranScript1.replace("ค่ะ", "");
+            newtranScript1 = newtranScript1.replace("จ้า", "");
+            newtranScript1 = newtranScript1.replace("จ้ะ", "");
+            newtranScript1 = newtranScript1.replace(" ", "");
+            console.log(newtranScript1);
+            if (newtranScript1.includes("สวัสดีน้องเกษม")) {
+                wake_word.onend = null;
+                wake_word.onresult = null;
+                wake_word.stop();
+                break;
+            }
+            else if (newtranScript1.includes("น้องเกษม")) {
+                wake_word.onend = null;
+                wake_word.onresult = null;
+                wake_word.stop();
+                break;
+            }
+        }
+    }
+}
+resetWakeWord();
+wake_word.start();
