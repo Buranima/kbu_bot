bdd3e49da23ccf7fca01f5b9dca53905372e3cd7
Buranima <lee123021@gmail.com>
2024-10-09 15:59:53 +0700

    new

---
+++ extension/auto.py
@@ -0,0 +1,9 @@
+from install_library import installLibrary
+from verify_folder import verifyFolder
+
+def autoHelp():
+    installLibrary()
+    verifyFolder()
+
+if __name__ == "__main__":
+    autoHelp()
--- extension/install_library.py
+++ extension/install_library.py
@@ -2,14 +2,15 @@ import json
 import subprocess
 import sys
 
-# อ่านไฟล์ JSON
-with open('static/config/install_config.json', 'r') as file:
-    data = json.load(file)
+def installLibrary():
+    with open('static/config/install_config.json', 'r') as file:
+        data = json.load(file)
 
-# ตรวจสอบว่าใน JSON มีคีย์ "extension_install" หรือไม่
-if "extension_install" in data:
-    packages = data["extension_install"]
-    
-    # ติดตั้งไลบรารีทั้งหมดที่ระบุในรายการ
-    for package in packages:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    if "extension_install" in data:
+        packages = data["extension_install"]
+
+        for package in packages:
+            subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+if __name__ == "__main__":
+    installLibrary()
