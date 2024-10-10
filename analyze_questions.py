15149fcf783f53626bcdd63a3b2a99c79e84362b
Buranima <lee123021@gmail.com>
2024-10-10 17:43:32 +0700

    all

--- analyze_questions.py
+++ analyze_questions.py
@@ -1,3 +1,4 @@
+# from pythainlp.tokenize import word_tokenize
 from pythainlp.tokenize import word_tokenize
 import os
 import json
@@ -8,6 +9,15 @@ latest_questions_file_path = "static/temp/latest_questions.json"
 questions_tokenize_file_path = "static/temp/questions_tokenize.json"
 result_questions_file_path = "static/temp/result_questions.json"
 kbubot_config_file_path = "static/config/json/kbubot_config.json"
+list_data_file_path = "static/temp/list_data.json"
+
+text_tts = ""
+
+list_data_json_answer = {
+    "question":[],
+    "answer": [],
+    "result":[]
+}
 
 if os.path.exists(questions_tokenize_file_path):
     pass
@@ -22,7 +32,7 @@ else:
     for id in range(len(json_database["id"])):
         data_json["id"].append(json_database["id"][id])
         data_json["question"].append(json_database["question"][id])
-        data_arry = word_tokenize(json_database["question"][id], engine="newmm")
+        data_arry = word_tokenize(json_database["question"][id], engine="deepcut")
         data_json["tokenize"].append(data_arry)
         data_json["answer"].append(json_database["answer"][id])
     with open(questions_tokenize_file_path, "w", encoding="utf-8") as json_file:
@@ -57,7 +67,7 @@ def findOne(text_questions_find_one):
 
     with open(questions_tokenize_file_path, "r", encoding="utf-8") as file_questions_tokenize:
         questions_tokenize_json = json.load(file_questions_tokenize)
-    list_text_questions = word_tokenize(text_questions_find_one, engine="newmm")
+    list_text_questions = word_tokenize(text_questions_find_one, engine="deepcut")
     for id in range(len(questions_tokenize_json["id"])):
         list_json_questions = questions_tokenize_json["tokenize"][id]
         len_questions_score = (len(list_text_questions) / len(list_json_questions)) * json_kbubot_config["lenpass"]
@@ -74,12 +84,12 @@ def findOne(text_questions_find_one):
         position_questions_score = (position_questions_score / len(list_json_questions)) * json_kbubot_config["positionpass"]
         sum_score = len_questions_score + like_questions_score + position_questions_score
 
-        print(list_json_questions)
-        print(list_text_questions)
-        print(f"Len: {len_questions_score}/10")
-        print(f"Like: {like_questions_score}/45")
-        print(f"Position: {position_questions_score}/45")
-        print(f"Score: {sum_score}/100")
+        # print(list_json_questions)
+        # print(list_text_questions)
+        # print(f"Len: {len_questions_score}/10")
+        # print(f"Like: {like_questions_score}/45")
+        # print(f"Position: {position_questions_score}/45")
+        # print(f"Score: {sum_score}/100")
 
         data_json["id"].append(questions_tokenize_json["id"][id])
         data_json["result"].append(sum_score)
@@ -91,6 +101,7 @@ def findOne(text_questions_find_one):
         json.dump(data_json, json_file, indent=4)
 
 def resultAnswer(text_result_answer):
+    global text_tts
 
     with open(questions_tokenize_file_path, "r", encoding="utf-8") as file_questions_tokenize:
         questions_tokenize_json = json.load(file_questions_tokenize)
@@ -105,28 +116,29 @@ def resultAnswer(text_result_answer):
     max_index = result_questions_json["result"].index(max_result)
     max_id = result_questions_json['id'][max_index]
     if max_result == 100.0:
-        print(f"\nคำถามคือ {text_result_answer}")
-        print(f'คำตอบคือ {questions_tokenize_json["answer"][max_id-1]}')
-        print(f"ID ที่มี Result score มากที่สุดคือ ID: {max_id} ด้วยค่า Result score ที่: {max_result}")
+        # print(f"\nคำถามคือ {text_result_answer}")
+        # print(f'คำตอบคือ {questions_tokenize_json["answer"][max_id-1]}')
+        # print(f"ID ที่มี Result score มากที่สุดคือ ID: {max_id} ด้วยค่า Result score ที่: {max_result}")
         setLatestQuestions(questions_tokenize_json["question"][max_id-1])
+        text_tts = questions_tokenize_json["answer"][max_id-1]
         return max_result
     elif max_result >= json_kbubot_config["passscore"]:
-        print(f"\nคำถามคือ {text_result_answer}")
-        print(f'คำตอบคือ {questions_tokenize_json["answer"][max_id-1]}')
-        print(f"ID ที่มี Result score มากที่สุดคือ ID: {max_id} ด้วยค่า Result score ที่: {max_result}")
+        # print(f"\nคำถามคือ {text_result_answer}")
+        # print(f'คำตอบคือ {questions_tokenize_json["answer"][max_id-1]}')
+        # print(f"ID ที่มี Result score มากที่สุดคือ ID: {max_id} ด้วยค่า Result score ที่: {max_result}")
         setLatestQuestions(questions_tokenize_json["question"][max_id-1])
         return max_result
     else:
-        print(f"\nคำถามคือ {text_result_answer}")
-        print(f'คำตอบที่ไกล้เคียงมากที่สุดคือ {questions_tokenize_json["answer"][max_id-1]}')
-        print(f"ID ที่มี Result score มากที่สุดคือ ID: {max_id} ด้วยค่า Result score ที่: {max_result}")
+        # print(f"\nคำถามคือ {text_result_answer}")
+        # print(f'คำตอบที่ไกล้เคียงมากที่สุดคือ {questions_tokenize_json["answer"][max_id-1]}')
+        # print(f"ID ที่มี Result score มากที่สุดคือ ID: {max_id} ด้วยค่า Result score ที่: {max_result}")
         setLatestQuestions("")
         return max_result
 
 def findAnswer(text_questions):
-    global questions_tokenize_file_path, result_questions_file_path, latest_questions_file_path
+    global questions_tokenize_file_path, result_questions_file_path, latest_questions_file_path, text_tts
 
-    list_text_questions = word_tokenize(text_questions, engine="newmm")
+    list_text_questions = word_tokenize(text_questions, engine="deepcut")
 
     result_score = 0.0
 
@@ -136,28 +148,41 @@ def findAnswer(text_questions):
     with open(questions_tokenize_file_path, "r", encoding="utf-8") as file_questions_tokenize:
         questions_tokenize_json = json.load(file_questions_tokenize)
 
+    with open(kbubot_config_file_path, "r", encoding="utf-8") as json_kbubot_config_file:
+                json_kbubot_config = json.load(json_kbubot_config_file)
+
     if os.path.exists(result_questions_file_path):
         if questions_latest_json["latest"] == "":
             findOne(text_questions)
             result_score = resultAnswer(text_questions)
+            if result_score == json_kbubot_config["resultpass"]:
+                return text_tts
+            elif result_score >= json_kbubot_config["passscore"]:
+                setLatestQuestions("")
+                text_tts = "หนูไม่พบคำตอบสำหรับคำถามนี้ค่ะ ผลลัพธ์การค้นหาที่ใกล้เคียงที่สุดคือ " + list_data_json_answer["answer"][max_index]
+                return text_tts
+            else:
+                text_tts = "หนูไม่เข้าใจคำถามค่ะ"
+                return text_tts
 
         else:
             findOne(text_questions)
 
             with open(result_questions_file_path, "r") as json_file:
                 result_questions_json = json.load(json_file)
-
-            with open(kbubot_config_file_path, "r", encoding="utf-8") as json_kbubot_config_file:
-                json_kbubot_config = json.load(json_kbubot_config_file)
             
             max_result = max(result_questions_json["result"])
             max_index = result_questions_json["result"].index(max_result)
             max_id = result_questions_json['id'][max_index]
-            if max_result >= json_kbubot_config["resultpass"]:
-                print(f"คำถามคือ {text_questions}")
-                print(f'คำตอบคือ {questions_tokenize_json["answer"][max_id-1]}')
-                print(f"ID ที่มี Result score มากที่สุดคือ ID: {max_id} ด้วยค่า Result score ที่: {max_result}")
+            if max_result >= json_kbubot_config["passscore"]:
+
+                # print(f"คำถามคือ {text_questions}")
+                # print(f'คำตอบคือ {questions_tokenize_json["answer"][max_id-1]}')
+                # print(f"ID ที่มี Result score มากที่สุดคือ ID: {max_id} ด้วยค่า Result score ที่: {max_result}")
+
                 setLatestQuestions(questions_tokenize_json["question"][max_id-1])
+                text_tts = questions_tokenize_json["answer"][max_id-1]
+                return text_tts
             else:
                 questions_index = questions_tokenize_json["question"].index(questions_latest_json["latest"])
                 edit_questions_tokenize_json = questions_tokenize_json["tokenize"][questions_index]
@@ -169,15 +194,62 @@ def findAnswer(text_questions):
                         # print(temp_list)
                         findOne("".join(temp_list))
                         result_score = resultAnswer("".join(temp_list))
-                        if result_score > json_kbubot_config["resultpass"]:
+
+                        with open(result_questions_file_path, "r") as json_file:
+                            result_questions_json = json.load(json_file)
+
+                        max_result = max(result_questions_json["result"])
+                        max_index = result_questions_json["result"].index(max_result)
+
+                        if "".join(temp_list) in questions_tokenize_json["question"]:
+                            max_index = questions_tokenize_json["question"].index("".join(temp_list))
+                            list_data_json_answer["answer"].append(questions_tokenize_json["answer"][max_index])
+                        else:
+                            list_data_json_answer["answer"].append(questions_tokenize_json["answer"][max_index])
+
+                        list_data_json_answer["question"].append(temp_list.copy())
+                        list_data_json_answer["result"].append(result_score)
+                        if result_score == json_kbubot_config["resultpass"]:
                             break
-                    if result_score > json_kbubot_config["resultpass"]:
+                    if result_score == json_kbubot_config["resultpass"]:
                         break
+
+                # print(f"{list_data_json_answer}\n")
+
+                max_result = max(list_data_json_answer["result"])
+                max_index = list_data_json_answer["result"].index(max_result)
+
+                # print(f'คำถามคือ {list_data_json_answer["question"][max_index]}')
+                # print(f'คำตอบคือ {list_data_json_answer["answer"][max_index]}')
+                # print(f"ID ที่มี Result score มากที่สุดคือ ID: {max_index + 1} ด้วยค่า Result score ที่: {max_result}")
+
+                with open(list_data_file_path, "w", encoding="utf-8") as json_file:
+                                    json.dump(list_data_json_answer, json_file, ensure_ascii=False, indent=4)
+
+                if result_score == json_kbubot_config["resultpass"]:
+                    setLatestQuestions("".join(list_data_json_answer["question"][max_index]))
+                    text_tts = list_data_json_answer["answer"][max_index]
+                    return text_tts
+                elif result_score >= json_kbubot_config["passscore"]:
+                    setLatestQuestions("")
+                    text_tts = "หนูไม่พบคำตอบสำหรับคำถามนี้ค่ะ ผลลัพธ์การค้นหาที่ใกล้เคียงที่สุดคือ " + list_data_json_answer["answer"][max_index]
+                    return text_tts
+                else:
+                    setLatestQuestions("")
+                    text_tts = "หนูไม่เข้าใจคำถามค่ะ"
+                    return text_tts
+
     else:
         findOne(text_questions)
         result_score = resultAnswer(text_questions)
+        if result_score == json_kbubot_config["resultpass"]:
+            return text_tts
+        else:
+            text_tts = "หนูไม่เข้าใจคำถามค่ะ"
+            return text_tts
 
 if __name__ == "__main__":
-    findAnswer("คณะวิศวกรรมศาสตร์มีทั้งหมดกี่สาขา")
-    # print(word_tokenize("แล้ววิศวกรรมเครื่องกลล่ะ", engine="newmm"))
-    # print(word_tokenize("แล้ววิศวกรรมซ่อมบำรุงล่ะ", engine="newmm"))
+    tts_result = findAnswer("เครื่องกลล่ะ")
+    print(f"\n{tts_result}")
+    # print(word_tokenize("วิศวกรรมโยธา", engine="deepcut"))
+    # print(word_tokenize("แล้ววิศวกรรมซ่อมบำรุงล่ะ", engine="deepcut"))
